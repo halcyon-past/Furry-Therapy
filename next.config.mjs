5fecf6b20de7d7fe58c1b00488a7f0ed45bc657b
halcyon-past <titanssuperior@gmail.com>
2024-09-03 14:56:39 +0530

    First Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: [
+        'mediaproxy.salon.com',
+        'www.akc.org',
+        'images.ctfassets.net',
+        'andy.pet',
+        'via.placeholder.com'
+      ],
+    },
+  };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
@@ -13,8 +14,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      clipPath: {
+        star: 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.star-clip-path': {
+          clipPath: 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)',
+        },
+      }
+      addUtilities(newUtilities)
+    }),
+  ],
 };
 export default config;
