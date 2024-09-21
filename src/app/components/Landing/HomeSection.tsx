c94509dc5f854130683a3f01d20699f755036e52
halcyon-past <titanssuperior@gmail.com>
2024-09-21 23:54:44 +0530

    rounded corners

--- src/app/components/Landing/HomeSection.tsx
+++ src/app/components/Landing/HomeSection.tsx
@@ -11,17 +11,17 @@ export default function HomeSection() {
     return (
         <div className="w-full min-h-screen pt-28 flex flex-col justify-between items-center pb-8">
             <div className="top w-11/12 max-w-5xl aspect-[3/2] relative mb-8 sm:aspect-[16/5]">
-                <p className="absolute bottom-5 left-1/2 -translate-x-1/2 bg-[#d7a46f55] z-10 w-10/12 py-2 px-4 text-center backdrop-blur-sm">
+                <p className="absolute bottom-5 left-1/2 -translate-x-1/2 rounded-lg bg-[#d7a46f55] z-10 w-10/12 py-2 px-4 text-center backdrop-blur-sm">
                     Swipe through and meet your new furry friend!
                 </p>
-                <Image src="/land-top.jpeg" alt="home" fill={true} className="object-cover" />
+                <Image src="/land-top.jpeg" alt="home" fill={true} className="object-cover rounded-lg" />
             </div>
             <div className="bottom flex flex-col justify-around items-center w-full space-y-8 sm:flex-row sm:space-y-0 sm:space-x-4 sm:w-11/12 max-w-5xl">
                 {SlideImages.map((section, index) => (
                     <div key={index} className="w-full max-w-[300px] flex flex-col items-center space-y-4">
                         <p className="text-[#a17d60] text-center">{section.title}</p>
                         <div className="w-full aspect-[3/2] relative">
-                            <Image src={section.image} alt={`section ${index + 1}`} fill={true} className="object-cover" />
+                            <Image src={section.image} alt={`section ${index + 1}`} fill={true} className="object-cover rounded-lg" />
                         </div>
                     </div>
                 ))}
