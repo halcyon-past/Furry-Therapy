5d202d0ce808168c4d86b801a0f4b3cb611e89e1
halcyon-past <titanssuperior@gmail.com>
2024-09-22 17:48:33 +0530

    Init Contact Us

--- src/app/components/Landing/ContactUs.tsx
+++ src/app/components/Landing/ContactUs.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function ContactUs() {
     return (
-        <div>Contact Us</div>
+        <div className="About w-full h-screen relative">
+            
+            <Image src="/about-us.jpeg" alt="home" fill={true} className="object-cover opacity-70" />
+        </div>
     )
 }
