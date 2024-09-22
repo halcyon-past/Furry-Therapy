ad66a4be591f48da49e3a9bdc29d4d94c0ff7ec5
halcyon-past <titanssuperior@gmail.com>
2024-09-22 17:56:35 +0530

    Added Contact Us

--- src/app/components/Landing/ContactUs.tsx
+++ src/app/components/Landing/ContactUs.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link';
 
 export default function ContactUs() {
     return (
-        <div className="About w-full h-screen relative">
-            
-            <Image src="/about-us.jpeg" alt="home" fill={true} className="object-cover opacity-70" />
+        <div className="About w-full h-screen relative flex flex-col justify-around items-center pt-20">
+            <div className="query w-8/12 h-[300px] bg-[#ffffff55] backdrop-blur-sm z-10 flex justify-center items-center text-[20px] rounded-lg p-5">
+                Have questions or wanted to get started? Reach out to us and we’ll be happy to assist you.
+            </div>
+            <Link href="mailto:aritro.saha2021@vitstudent.ac.in" target='_blank' className='w-36 h-20 bg-[#ffffff55] backdrop-blur-sm z-10 flex justify-center items-center text-[20px] rounded-lg'>Email Us</Link>
+            <Image src="/contact-us.jpeg" alt="home" fill={true} className="object-cover opacity-70" />
         </div>
     )
 }
