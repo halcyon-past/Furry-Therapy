a96ec944369c2ec083c9fb6637a42b57a563613f
halcyon-past <titanssuperior@gmail.com>
2024-10-12 00:54:04 +0530

    changed order of status check at init

--- src/app/init/page.tsx
+++ src/app/init/page.tsx
@@ -6,17 +6,17 @@ export default function Init() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  if (status === 'unauthenticated') {
-    router.push('/');
-    return null;
-  }
-
   if (status === 'loading') {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
       </div>
     );
+  }
+
+  if (status === 'unauthenticated') {
+    router.push('/');
+    return null;
   }
 
   const handlePetOwnerRegistration = () => {
