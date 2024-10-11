d92eabd6e1205bd5a44ee0c93846d08ec8c4bc03
halcyon-past <titanssuperior@gmail.com>
2024-10-11 23:47:14 +0530

    Changed order of status call in date page

--- src/app/date/page.tsx
+++ src/app/date/page.tsx
@@ -93,17 +93,17 @@ export default function Date() {
   const router = useRouter();
   const [currentAnimalIndex, setCurrentAnimalIndex] = React.useState(0);
 
-  if (status === 'unauthenticated') {
-    router.push('/');
-    return null;
-  }
-
   if (status === 'loading') {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
       </div>
     );
+  }
+
+  if (status === 'unauthenticated') {
+    router.push('/');
+    return null;
   }
 
   const currentAnimal = animals[currentAnimalIndex];
